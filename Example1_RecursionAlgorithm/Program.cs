4cf119fba2b6b1907f9e66815bf63809154edc80
Sergei <radchenkovsa@yandex.ru>
2023-08-31 16:16:50 +0300

    Example_RecursionAlgorithm

---
+++ Example1_RecursionAlgorithm/Program.cs
@@ -0,0 +1,22 @@
+﻿/* string[,] table = new string[2, 5];
+table[1, 2] = "слово";
+for (int rows = 0; rows < 2; rows++)
+{
+    for (int columns = 0; columns < 5; columns++)
+    {
+        Console.WriteLine($"-{table[rows, columns]}-");
+    }
+} */ 
+
+void PrintArray(int[,] matr)
+{
+    int[,] matr = new int[3, 4];
+        for (int i = 0; i < matr.GetLength(0); i++)
+        {
+            for (int j = 0; j < matr.GetLength(1); j++)
+            {
+                 Console.Write($"{matr[i, j]} ");
+            }
+            Console.WriteLine();
+        }
+}
